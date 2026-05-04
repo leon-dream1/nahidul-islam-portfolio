b4d9422ab906b1627cd08c1c722f4f04c1834d87
leon-dream1 <nahidulislam019045@gmail.com>
2026-05-04 15:28:46 +0600

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://nahidul-islam-portfolio.vercel.app/",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
